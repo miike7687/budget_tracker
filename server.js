dca64cb7e727383636b7d90e6d0df8fe057e53e8
Michael Haber <michaeladam.haber@gmail.com>
2020-08-24 19:40:56 -0400

    Changed my connection URL

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
 });
 
 // routes
