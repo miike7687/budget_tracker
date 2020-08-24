662d8a6abfb78902f8a2ac77fa97b690a08d80ac
Michael Haber <michaeladam.haber@gmail.com>
2020-08-24 15:09:59 -0400

    Added a serviceworker js file

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/models/transaction.js",
+  "/manifest.webmanifest",
+  "/assets/images/icons/icon-192x192.png",
+  "/assets/images/icons/icon-512x512.png",
+];
